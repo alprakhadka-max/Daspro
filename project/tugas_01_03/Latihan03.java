1b997e11abd9720644b309d52ff8d6693793a224
AlpratamaReqzama <alprakhadka@gmail.com>
2026-09-02 17:24:40 +0700

    Delete project/tugas_01_03 directory

--- project/tugas_01_03/Latihan03.java
+++
@@ -1,9 +0,0 @@
-public class Latihan03 {
-
-    public static void main(String[] args) {
-        System.out.println("Halo, nama saya adalah Alpratama!");
-
-        System.out.print("\nBelajar Java itu mudah!");
-        //System.out.print("\nSemakin sering mecoba, semakin jago!");
-    }
-}

0dc8cf0f924320140b3b1a44b7f6303e449f869b
AlpratamaReqzama <alprakhadka@gmail.com>
2026-09-02 17:25:18 +0700

    Add files via upload

---
+++ project/jobsheet01/RobotArm03.java
@@ -0,0 +1,11 @@
+public class RobotArm03 {
+    public static void main(String[] args) {
+        
+       
+        System.out.println("Setelah Langkah 1: Nampan A = kosong, Nampan B = bulan, Nampan C = bintang");
+        System.out.println("Setelah Langkah 2: Nampan A = bulan, Nampan B = kosong, Nampan C = bintang");
+        System.out.println("Setelah Langkah 3: Nampan A = bulan, Nampan B = bintang, Nampan C = kosong");
+        System.out.println("Kesimpulan: Pernyataan yang benar adalah (a) Kedua Bola berpindah tempat dan (e) Nampan C Kosong"); 
+     }
+}
+
---
+++ project/jobsheet02/contohvariabel03.java
@@ -0,0 +1,17 @@
+public class contohvariabel03 {
+    public static void main(String[] args) {
+        String salahsatuhobysayaadalah = ("bermain petak umpet");
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte _umursayasekarang = 20;
+        double $ipk = 3.24, tinggi = 1.78;
+        System.out.println(salahsatuhobysayaadalah);
+        System.out.println("apakah Pandai : ? " + isPandai);
+        System.out.println("jenis kelamin: " + jenisKelamin);
+        System.out.println("umurku saat ini: " + _umursayasekarang);
+        System.out.println(String.format("saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+
+    
+}
